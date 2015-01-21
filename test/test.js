549fcf9e161230eee3f8270972d4c299f3cc74db
Kyle Stetz <kyle@punkave.com>
2015-01-21 14:49:05 -0500

    added tests for existence of basic methods

--- test/test.js
+++ test/test.js
@@ -2,11 +2,35 @@ var assert = require('assert');
 
 describe('synthesizer', function() {
   it('should exist', function(done) {
-    var synth = require('../index.js')({
-
-    });
+    var synth = require('../index.js')({ });
 
     assert(synth);
     return done();
+  });
+
+  describe('methods', function() {
+    it('should have a `define` method', function(done) {
+      var synth = require('../index.js')({});
+      assert(synth.define);
+      return done();
+    });
+
+    it('should have a `redefine` method', function(done) {
+      var synth = require('../index.js')({});
+      assert(synth.redefine);
+      return done();
+    });
+
+    it('should have a `create` method', function(done) {
+      var synth = require('../index.js')({});
+      assert(synth.create);
+      return done();
+    });
+
+    it('should have a `isDefined` method', function(done) {
+      var synth = require('../index.js')({});
+      assert(synth.isDefined);
+      return done();
+    });
   });
 });
